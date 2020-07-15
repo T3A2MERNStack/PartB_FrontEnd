90a0d8b8dd99787b2ab56575484949d595e04806
highpitchit <ameliafrancesnunn@gmail.com>
2020-07-15 11:42:21 +1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div >
+          Welcome to EcoA - your resource for zero waste lifestyle recipes 
+    </div>
+  )
+}
+
+export default App
