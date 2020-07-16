e8e3dd22fc25a8c9021d3cfe19a2af9a0383f16c
ArisaOkuyama <arisaokuyama@gmail.com>
2020-07-16 15:38:14 +1000

    added create button

--- src/WelcomePage.js
+++ src/WelcomePage.js
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Axios from 'axios'
 
 export default function Home(props) {
-    const onSubmit = () => {
-        Axios.post("http://localhost:4000/lists", {"name":"test recipe", "duration":"345678"})
+    const [recipe, getRecipe] = useState("")
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        Axios.post("http://localhost:4000/lists", {"name":"TEST", "duration":"1111"})
         .then(res => console.log(res))
         .catch(err => console.log(err))
     }
+
+    useEffect(() => {
+        Axios.get("http://localhost:4000/lists")
+        .then(res => {
+            getRecipe(res.data.map(value=> value.name))
+        })
+        .catch(err => console.log(err))
+    },[])
+
     return (
         <div>
             <h1> Welcome to EcoA - your resource for zero waste home products</h1>
             <form onSubmit={onSubmit}>
-                <button>Get all recipe</button>
+                <button>Add test recipe</button>
             </form>
+            <p>{recipe}</p>
+            {console.log(recipe)}
         </div>
     )
 }
