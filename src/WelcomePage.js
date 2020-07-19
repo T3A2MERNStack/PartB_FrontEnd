f05ce030c4c36f007640ab912c5a28445f495d73
highpitchit <ameliafrancesnunn@gmail.com>
2020-07-19 23:11:02 +1000

    configuring welcome page

--- src/WelcomePage.js
+++ src/WelcomePage.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import Axios from 'axios'
 import { useHistory } from 'react-router-dom'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from 'react-bootstrap/Container'
 
 export default function Home(props) {
     const [recipe, getRecipe] = useState("")
@@ -24,12 +26,29 @@ export default function Home(props) {
     
 
     return (
+       <>
         <div>
-            <h1> Welcome to EcoA - your resource for zero waste home products</h1>
+            <Jumbotron >
+                <Container >
+                    <h1>Welcome to EcoA</h1>
+                    <p>
+                        Your resource for zero waste home products
+                    </p>
+                </Container>
+            </Jumbotron>
+        </div> 
             <form onSubmit={onSubmit}>
                 <button>Add test recipe</button>
             </form>
-            {/* {console.log(recipe)} */}
+            <div class="card w-1">
+        <div class="card-body">
+            <h5 class="card-title">Skincare</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="/skincare" class="card-link">Card link</a>
         </div>
+        </div>
+            {/* {console.log(recipe)} */}
+        </>  
     )
 }
