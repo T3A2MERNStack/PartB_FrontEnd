0d26f51089331edabe48743f682a6ba8599f956e
ArisaOkuyama <arisaokuyama@gmail.com>
2020-07-21 20:33:40 +1000

    changed the code to glens passport version

--- src/WelcomePage.js
+++ src/WelcomePage.js
@@ -1,28 +1,29 @@
 import React, { useEffect, useState } from 'react'
 import Axios from 'axios'
-import { useHistory } from 'react-router-dom'
+import { useHistory, Route, BrowserRouter, Link } from 'react-router-dom'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Container from 'react-bootstrap/Container'
 import styled from 'styled-components'
+import LoginPageView from './LoginPage'
 
 export default function Home(props) {
-    const [recipe, getRecipe] = useState("")
-    const history = useHistory();
+    // const [recipe, getRecipe] = useState("")
+    // const history = useHistory();
 
-    const onSubmit = (e) => {
-        e.preventDefault()
-        Axios.post("http://localhost:4000/lists", {"name":"TEST", "duration":"1111"})
-        .then(res => console.log(res))
-        .catch(err => console.log(err))
-    }
+    // const onSubmit = (e) => {
+    //     e.preventDefault()
+    //     Axios.post("http://localhost:4000/lists", {"name":"TEST", "duration":"1111"})
+    //     .then(res => console.log(res))
+    //     .catch(err => console.log(err))
+    // }
 
-    useEffect(() => {
-        Axios.get("http://localhost:4000/lists")
-        .then(res => {
-            getRecipe(res.data.map(value=> value.name))
-        })
-        .catch(err => console.log(err))
-    },[])
+    // useEffect(() => {
+    //     Axios.get("http://localhost:4000/lists")
+    //     .then(res => {
+    //         getRecipe(res.data.map(value=> value.name))
+    //     })
+    //     .catch(err => console.log(err))
+    // },[])
 
     
 
@@ -38,10 +39,11 @@ export default function Home(props) {
                 </Container>
             </Jumbotron>
         </div>
-
-            <form onSubmit={onSubmit}>
+        {/* <Link to="/signup">Sign up</Link> */}
+        <Link to="/login">Log in</Link>
+            {/* <form onSubmit={onSubmit}>
                 <button>Add test recipe</button>
-            </form>
+            </form> */}
         <div className="container" style={{backgroundColor: 'grey'}}>
             <div className="row">
                 <div className="col" style={{backgroundColor: 'red', maxWidth : '40%', marginLeft : '5%', marginRight : '5%', minHeight : '200px'}}>
--- src/stateReducer.js
+++ src/stateReducer.js
@@ -1,8 +1,14 @@
 export default function (state, action){
     switch (action.type){
         case "setLogin": {
+            console.log("setLogin action: " + action.data)
             return {
                 login: action.data
+            }
+        }
+        case "setUser" : {
+            return {
+                data: action.data
             }
         }
         default: 
