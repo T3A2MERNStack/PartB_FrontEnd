e0f513d1b2df710cf6771e3b2ffd0cb1f9b4794f
highpitchit <ameliafrancesnunn@gmail.com>
2020-07-22 10:45:33 +1000

    cloudinary pack added

--- src/NewRecipeView.js
+++ src/NewRecipeView.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './App.css'
+import {Image} from 'cloudinary-react'
 
 class NewRecipeView extends React.Component{
     state = {
