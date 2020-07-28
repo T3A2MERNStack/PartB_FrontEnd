9f936a2bd58a9f5a47a15b3be4a70b7b884c4ff5
highpitchit <ameliafrancesnunn@gmail.com>
2020-07-28 19:59:03 +1000

    added in filter pages

--- src/pages/PersonalCarePage.js
+++ src/pages/PersonalCarePage.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Image } from 'cloudinary-react';
+import Axios from 'axios'
+import { Card, Icon, Rating } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+import { Container, Row , Col} from 'react-bootstrap'
 
-export default function PersonalCarePage() {
+
+export default function SkincarePage() {
+
+    const [data, setData] = useState("")
+    const url = "http://localhost:4000"
+    const category = "personal"
+
+    useEffect(() => {
+        Axios.get(`${url}/recipes/categorypc/${category}`)
+            .then(res => {
+                setData(res.data)
+                console.log(res.data)
+            })
+            .catch(error => {
+                if(error) {
+                console.log(error.message)
+            }})
+        },[]);
+    // console.log(typeof(data))
+    const newArray = Array.from(data)
+
+
     return (
-        <div>
-            <h1>Personal Care Recipes</h1>
-        </div>
+        <>
+        <Container>
+            <Row  className="justify-content">
+                
+                {
+                    newArray.map((object, index) => {
+                        return (
+                            <Col sm={12} md={3}>
+                            <Link to={`/recipe/${object._id}`} key={`${object._id}`} >
+                                <Card key={`${index}`} style={{margin: 10}}>
+                                    <Image alt="product" cloudName="highpitchit" dpr="auto" publicId={object._id} width="150" crop="scale"/>
+                                    <Card.Content>
+                                        <Card.Header>{object.productName}</Card.Header>
+                                        <Card.Meta>
+                                            <span className='date'>User</span>
+                                        </Card.Meta>
+                                        <Card.Description>
+                                            {object.productSummary}
+                                        </Card.Description>
+                                    </Card.Content>
+                                    <Card.Content extra>
+                                        <Rating icon='heart' defaultRating={3} maxRating={5} /> (### ratings)
+                                    </Card.Content>
+                                </Card>
+                            </Link>                
+                        </Col>
+                    )})     
+                }
+            </Row>
+        </Container>
+    </>
     )
 }
