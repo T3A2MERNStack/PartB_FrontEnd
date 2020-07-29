79c9afeb4647182d98a7a10786bb7fff9cbaa6eb
ArisaOkuyama <arisaokuyama@gmail.com>
2020-07-29 10:02:29 +1000

    added myrecipe page

--- src/pages/MyRecipesPage.js
+++ src/pages/MyRecipesPage.js
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useEffect, useState, useContext } from 'react'
+import { Image } from 'cloudinary-react';
+import Axios from 'axios'
+import { Card, Icon, Rating } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+import { Container, Row , Col, Jumbotron} from 'react-bootstrap'
+import StateContext from '../store'
 
-export default function MyRecipePage() {
+export default function MyRecipe() {
+    const {store, dispatch} = useContext(StateContext)
+    const [data, setData] = useState(false)
+    const url = "http://localhost:4000"
+
+    // useEffect(() => {
+    //     console.log(store.recipe)
+    //     setData(store.recipe)
+
+    // },[]);
+    console.log(store.recipe)
+    
+    // setData(store.recipe)
+    // const newArray = Array(store.recipe)
+
     return (
-        <div>
-            <h1>List of Users Recipes</h1>
-        </div>
+        store.recipe ? (
+        <>
+            <Jumbotron className="text-center" style={{ marginTop: '30px'}}>
+            <Container>
+                        <h1>Personal Care Recipes</h1>
+            </Container>
+            </Jumbotron>
+            <Container>
+                <Row  className="justify-content">
+                    
+                    {
+                        store.recipe.map((object, index) => {
+                            return (
+                                <Col sm={12} md={3} key={`${object._id}`}>
+                                <Link to={`/recipe/${object._id}`} key={`${object._id}`} >
+                                    <Card style={{margin: 10, width: "250px", height: "400px"}}>
+                                        <Image alt="product" cloudName="highpitchit" dpr="auto" publicId={object._id} width="150" crop="scale"/>
+                                        <Card.Content>
+                                            <Card.Header>{object.productName}</Card.Header>
+                                            <Card.Meta>
+                                                <span className='date'>User</span>
+                                            </Card.Meta>
+                                            <Card.Description>
+                                                {object.productSummary}
+                                            </Card.Description>
+                                        </Card.Content>
+                                        <Card.Content extra>
+                                            <Rating icon='heart' defaultRating={3} maxRating={5} /> (### ratings)
+                                        </Card.Content>
+                                    </Card>
+                                </Link>                
+                            </Col>
+                        )})     
+                    }
+                </Row>
+            </Container>
+        </>
+        ): (<Container>YOU DONT HAVE RECIPE</Container>)
     )
 }
+
