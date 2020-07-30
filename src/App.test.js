90a280abeca2ed8ef59955548f950479813ee943
highpitchit <ameliafrancesnunn@gmail.com>
2020-07-30 11:34:44 +1000

    added testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import { render } from '@testing-library/react';
 import App from './App';
-import Enzyme, { shallow } from "enzyme"
+import Enzyme, { shallow, mount } from "enzyme"
 import EnzymeAdapter from 'enzyme-adapter-react-16'
 import Display from './pages/DisplayRecipePage'
 
@@ -13,6 +13,9 @@ import { Link } from 'react-router';
 import ShowPage from './pages/ShowPage'
 import SignupPageView from './pages/SignupPage'
 import NewRecipeView from './pages/candelete'
+import NavBar from './pages/NavBar'
+import WelcomePage from './pages/WelcomePage'
+import NoMatch from './pages/NoMatch'
 
 
 Enzyme.configure({ adapter: new EnzymeAdapter() })
@@ -38,42 +41,14 @@ it ('skin care page renders without crashing', () => {
   const wrapper = shallow(<SkincarePage />)
 })
 
-it ('new recipe page renders without crashing', () => {
+it ('new recipe page shows error message', () => {
   const wrapper = shallow(<NewRecipeView />)
   expect(wrapper.find('YOU MUST LOG IN TO CREATE RECIPES'))
 })
 
-
-
-
-
-//tests not working
-
-it ('login button clicks', () => {
-  const wrapper = shallow(<ShowPage/>)
-  expect(wrapper.find('[type="submit"]').simulate("click"))
+it('finds no match error ', () => {
+  const wrapper = shallow(<NoMatch />)
+  expect(wrapper.find('h1').text()).toContain('Error 404 Page does not exist')
 })
 
-it ('login button clicks', () => {
-  const wrapper = shallow(<SkincarePage />)
-  expect(wrapper.find('.data')).to.have.lengthOf(1);
-  wrapper.setState({ name: 'data' })
-})
-
-it('includes link to skincare page', () => {                                       
-  const wrapper = shallow(<SkincarePage/>);
-  expect(wrapper.find(Link).props().to).toBe('/skin');
- });
-
- it("displays steps", () => {
-  const wrapper = mount(<ShowPage />);
-  expect(wrapper.find("STEP"))
-});
-
-describe('LoginMethods()', () => {
-  it('isEmailValid should return false if email is invalid', () => {
-    expect(LoginMethods().isEmailValid('notvalidemail')).toBeFalsy();
-    expect(LoginMethods().isEmailValid('notvalidemail.aswell')).toBeFalsy();
-  })
-});
 
